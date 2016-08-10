1ed65dd3a1246d419cefa5c5f6af9ae5cd74cd3f
rafrex <code@rafrex.com>
2016-08-10 14:45:36 -0700

    Add event handler

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,18 @@ class ReactInteractive extends React.Component {
       PropTypes.element,
       PropTypes.array,
     ]),
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    onMouseMove: PropTypes.func,
+    onMouseDown: PropTypes.func,
+    onMouseUp: PropTypes.func,
+    onTouchStart: PropTypes.func,
+    onTouchEnd: PropTypes.func,
+    onTouchCancel: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    onKeyUp: PropTypes.func,
   }
 
   constructor(props) {
@@ -55,16 +67,85 @@ class ReactInteractive extends React.Component {
 
   handleEvent = (e) => {
     console.log(e.type);
+    console.log(e);
+
+    switch (e.type) {
+      case 'mousenter':
+        this.props.onMouseEnter && this.props.onMouseEnter(e);
+        this.track.mouseOn = true;
+        this.track.buttonDown = e.buttons === 1;
+        break;
+      case 'mouseleave':
+        this.props.onMouseLeave && this.props.onMouseLeave(e);
+        this.track.mouseOn = false;
+        this.track.buttonDown = false;
+        break;
+      case 'mousemove':
+        this.props.onMouseMove && this.props.onMouseMove(e);
+        // early return for mouse move
+        if (this.track.mouseOn && this.track.buttonDown === (e.buttons === 1)) return;
+        this.track.mouseOn = true;
+        this.track.buttonDown = e.buttons === 1;
+        break;
+      case 'mousedown':
+        this.props.onMouseDown && this.props.onMouseDown(e);
+        this.track.mouseOn = true;
+        this.track.buttonDown = true;
+        break;
+      case 'mouseup':
+        this.props.onMouseUp && this.props.onMouseUp(e);
+        this.track.buttonDown = false;
+        break;
+      case 'touchstart':
+        this.props.onTouchStart && this.props.onTouchStart(e);
+        this.track.touchDown = true;
+        this.track.touchStartTime = Date.now();
+        break;
+      case 'touchend':
+        this.props.onTouchEnd && this.props.onTouchEnd(e);
+        this.track.touchDown = false;
+        this.track.touchEndTime = Date.now();
+        break;
+      case 'touchcancel':
+        this.props.onTouchCancel && this.props.onTouchCancel(e);
+        this.track.touchDown = false;
+        this.track.touchEndTime = Date.now();
+        break;
+      case 'focus':
+        this.props.onFocus && this.props.onFocus(e);
+        this.track.focus = true;
+        break;
+      case 'blur':
+        this.props.onBlur && this.props.onBlur(e);
+        this.track.focus = false;
+        break;
+      case 'keydown':
+        this.props.onKeyDown && this.props.onKeyDown(e);
+        if (e.key === ' ') this.track.spaceKeyDown = true;
+        else if (e.key === 'Enter') this.track.enterKeyDown = true;
+        break;
+      case 'keyup':
+        this.props.onKeyUp && this.props.onKeyUp(e);
+        if (e.key === ' ') this.track.spaceKeyDown = false;
+        else if (e.key === 'Enter') this.track.enterKeyDown = false;
+        break;
+      default:
+        return;
+    }
+
+    // TODO
   }
 
   handleHybridMouseEvent = (e) => {
     console.log(e.type);
+    !this.track.touchDown && (Date.now() - this.track.touchEndTime > 600) && this.handleEvent(e);
   }
 
   render() {
     const { as } = this.props;
     const children = this.state.iState; // for testing purposes
     const props = this.listeners;
+    props.tabIndex = 1;
     return React.createElement(as, props, children);
   }
 }
