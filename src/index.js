f7a7a49e26f21c5b6c532eb77b5a680e7b25f746
rafrex <code@rafrex.com>
2016-10-25 09:41:04 -0700

    Move compare props to its own file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import detectIt from 'detect-it';
 import objectAssign from 'object-assign';
+import compareProps from './compareProps';
 
 class ReactInteractive extends React.Component {
   static propTypes = {
@@ -163,7 +164,7 @@ class ReactInteractive extends React.Component {
 
     // check if nextProps are the same as this.props
     this.p.sameProps = false;
-    if (!nextProps.mutableProps && this.sameProps(this.props, nextProps)) this.p.sameProps = true;
+    if (!nextProps.mutableProps && compareProps(this.props, nextProps)) this.p.sameProps = true;
     // if not same props, do props setup => set properties of `this.p`
     else this.propsSetup(nextProps);
 
@@ -262,77 +263,6 @@ class ReactInteractive extends React.Component {
         this.p.props.refDOMNode && this.p.props.refDOMNode(this.topNode);
       }
     }
-  }
-
-  // shallow compare of two sets of props, can be called recursivly
-  sameProps(propsA, propsB) {
-    // If children are ReactElements, e.g. JSX as opposed to strings,
-    // they will not be equal even if they are the same because React.createElement(...)
-    // returns a new instance every time and is usually called on every render,
-    // so unless there is a deep compare of the ReactElement tree of children,
-    // it doesn't make sense to continue checking other props.
-    // Note, that when nothing has changed in props,
-    // the only thing that's not equal are the children, so check first.
-    if (propsA.children !== propsB.children) return false;
-
-    const keysB = Object.keys(propsB);
-
-    // don't include forceState when comparing props
-    // forceState is handled in componentWillReceiveProps
-    const nextPOffset = propsB.forceState ? -1 : 0;
-    const pOffset = propsA.forceState ? -1 : 0;
-    if ((keysB.length + nextPOffset) !== (Object.keys(propsA).length + pOffset)) return false;
-
-    // shallow compare of state props => check style, className, onEnter, onLeave
-    const sameStateProp = (stateProp) => {
-      const statePropKeys = (stateProp !== 'focus') ? ['style', 'className', 'onEnter', 'onLeave'] :
-      ['style', 'className', 'onEnter', 'onLeave', 'focusFromOnly', 'focusFromTabStyle',
-      'focusFromMouseStyle', 'focusFromTouchStyle', 'focusFromTabClassName',
-      'focusFromMouseClassName', 'focusFromTouchClassName'];
-      if (!statePropKeys.some(key => propsB[stateProp][key])) return false;
-      if (statePropKeys.some(key => propsB[stateProp][key] !== propsA[stateProp][key])) {
-        return false;
-      }
-      return true;
-    };
-    // list of state props to compare one level deeper
-    const stateProps = {
-      normal: true,
-      hover: true,
-      active: true,
-      hoverActive: true,
-      touchActive: true,
-      keyActive: true,
-      focus: true,
-    };
-
-    // loop through props
-    for (let i = 0; i < keysB.length; i++) {
-      // skip if prop is forceState
-      if (keysB[i] !== 'forceState') {
-        // do propsA and propsB both have the property as their own?
-        if (!Object.prototype.hasOwnProperty.call(propsA, keysB[i])) return false;
-        // if the two props aren't equal, do some additional checks before returning false
-        if (propsB[keysB[i]] !== propsA[keysB[i]]) {
-          if (keysB[i] === 'as') {
-            if (React.isValidElement(propsA.as) && React.isValidElement(propsB.as)) {
-              // If `as` is JSX/ReactElement, first check to see if `as.type` is the same,
-              // e.g. 'div', 'span', ReactClass, ReactFunctionalComponent, and then shallowly
-              // compare it's props with a recursive call to sameProps - this should only recurse
-              // one time because the JSX/ReactElement shouldn't have the `as` prop.
-              if (propsA.as.type !== propsB.as.type) return false;
-              if (!this.sameProps(propsA.as.props, propsB.as.props)) return false;
-            } else {
-              return false;
-            }
-            // if the prop is a state, check one level deeper if the props are equal
-          } else if (!(stateProps[keysB[i]] && sameStateProp(keysB[i]))) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
   }
 
   // setup `this.p`, only called from constructor and componentWillReceiveProps
