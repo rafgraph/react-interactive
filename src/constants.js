9efe73cf926e37b827984fe0dabe2d2a0f98a379
rafrex <code@rafrex.com>
2016-10-26 16:21:45 -0700

    Create constants file

---
+++ src/constants.js
@@ -0,0 +1,74 @@
+export const iStates = {
+  normal: true,
+  hover: true,
+  hoverActive: true,
+  touchActive: true,
+  keyActive: true,
+};
+
+export const focusFromStates = {
+  tab: true,
+  mouse: true,
+  touch: true,
+};
+
+const iStateProps = {
+  ...iStates,
+  active: true,
+};
+
+export const stateProps = {
+  ...iStateProps,
+  focus: true,
+};
+
+export const iStateOptionsKeys = ['style', 'className', 'onEnter', 'onLeave'];
+
+export const focusOptionsKeys = [
+  ...iStateOptionsKeys,
+  'focusFromOnly',
+  'focusFromTabStyle',
+  'focusFromMouseStyle',
+  'focusFromTouchStyle',
+  'focusFromTabClassName',
+  'focusFromMouseClassName',
+  'focusFromTouchClassName',
+];
+
+
+// known props to not pass through, every prop not on this list is passed through
+export const knownProps = {
+  ...stateProps,
+  children: true,
+  as: true,
+  style: true,
+  className: true,
+  onStateChange: true,
+  setStateCallback: true,
+  onClick: true,
+  onMouseClick: true,
+  onEnterKey: true,
+  onTap: true,
+  onTapTwo: true,
+  onTapThree: true,
+  onTapFour: true,
+  onMouseEnter: true,
+  onMouseLeave: true,
+  onMouseMove: true,
+  onMouseDown: true,
+  onMouseUp: true,
+  onTouchStart: true,
+  onTouchEnd: true,
+  onTouchCancel: true,
+  onFocus: true,
+  onBlur: true,
+  onKeyDown: true,
+  onKeyUp: true,
+  forceState: true,
+  initialState: true,
+  refDOMNode: true,
+  mutableProps: true,
+  useBrowserWebkitTapHighlightColor: true,
+  useBrowserOutlineFocus: true,
+  useBrowserCursor: true,
+};
