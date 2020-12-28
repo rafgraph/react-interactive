8b58a8af01b3b5c2c9ee8f0f08a72b22f2e616db
Rafael Pedicini <rafael@rafgraph.dev>
2020-12-27 20:19:47 -0500

    Move polymorphic as prop typing to its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { eventFrom } from 'event-from';
+import { PolymorphicComponentProps } from './polymorphicAsType';
 
 export type ActiveState = 'mouseActive' | 'touchActive' | 'keyActive' | false;
 export type FocusState =
@@ -32,64 +33,6 @@ const stateChanged = ({ state, prevState }: InteractiveStateChange) =>
   state.active !== prevState.active ||
   state.focus !== prevState.focus ||
   state.disabled !== prevState.disabled;
-
-/////////////////////////////////////////////////////////////////////
-// polymorphic as prop typing adapted from:
-// https://www.benmvp.com/blog/polymorphic-react-components-typescript/
-// how to implement forwardRef with polymorphic typing in the Interactive component adapted from:
-// https://github.com/kripod/react-polymorphic-box#forwarding-refs
-/////////////////////////////////////////////////////////////////////
-// Source: https://github.com/emotion-js/emotion/blob/master/packages/styled-base/types/helper.d.ts
-// A more precise version of just React.ComponentPropsWithRef on its own
-export type PropsOf<
-  C extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
-> = JSX.LibraryManagedAttributes<C, React.ComponentPropsWithRef<C>>;
-
-// rafgraph note: 'as' has to be an optional property for some reason
-// if it's required then TS thinks the 'as' prop in Interactive is:
-// var as: C extends React.ElementType<any> = "button"
-// which results in TS error:
-// Type ...(long and complex)... not assignable to type 'LibraryManagedAttributes<C, any>'
-// compared to if 'as' is optional, then TS thinks the 'as' prop in Interactive is:
-// var as: C | undefined
-// which TS treats correctly
-type AsProp<C extends React.ElementType> = {
-  /**
-   * An override of the default HTML tag.
-   * Can also be another React component.
-   */
-  as?: C;
-};
-
-/**
- * Allows for extending a set of props (`ExtendedProps`) by an overriding set of props
- * (`OverrideProps`), ensuring that any duplicates are overridden by the overriding
- * set of props.
- */
-export type ExtendableProps<
-  ExtendedProps = Record<string, never>,
-  OverrideProps = Record<string, never>
-> = OverrideProps & Omit<ExtendedProps, keyof OverrideProps>;
-
-/**
- * Allows for inheriting the props from the specified element type so that
- * props like children, className & style work, as well as element-specific
- * attributes like aria roles. The component (`C`) must be passed in.
- */
-export type InheritableElementProps<
-  C extends React.ElementType,
-  Props = Record<string, never>
-> = ExtendableProps<PropsOf<C>, Props>;
-
-/**
- * A more sophisticated version of `InheritableElementProps` where
- * the passed in `as` prop will determine which props can be included
- */
-export type PolymorphicComponentProps<
-  C extends React.ElementType,
-  Props = Record<string, never>
-> = InheritableElementProps<C, Props & AsProp<C>>;
-/////////////////////////////////////////////////////////////////////
 
 interface InteractiveProps {
   // don't add 'as' prop to interface, it is provided by type AsProp as part of PolymorphicComponentProps
