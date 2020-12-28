a9bb7ba2611abefdf9b5a747d5bd9da13b82a059
Rafael Pedicini <rafael@rafgraph.dev>
2020-12-28 15:15:07 -0500

    Memoize Interactive with React.memo

--- src/index.tsx
+++ src/index.tsx
@@ -63,196 +63,206 @@ interface InteractiveProps {
 
 export const Interactive: <C extends React.ElementType = 'button'>(
   props: PolymorphicComponentProps<C, InteractiveProps>,
-) => React.ReactElement | null = React.forwardRef(
-  <C extends React.ElementType = 'button'>(
-    {
-      as,
-      children,
-      onStateChange,
-      ...restProps
-    }: PolymorphicComponentProps<C, InteractiveProps>,
-    ref: typeof restProps.ref,
-  ) => {
-    const As = as || 'button';
+) => React.ReactElement | null =
+  // TODO get displayName working with polymorphic props, maybe use new lib https://github.com/kripod/react-polymorphic-types
+  // eslint-disable-next-line react/display-name
+  React.memo(
+    React.forwardRef(
+      <C extends React.ElementType = 'button'>(
+        {
+          as,
+          children,
+          onStateChange,
+          ...restProps
+        }: PolymorphicComponentProps<C, InteractiveProps>,
+        ref: typeof restProps.ref,
+      ) => {
+        const As = as || 'button';
 
-    const [iState, setIState] = React.useState<InteractiveStateChange>({
-      state: initialState,
-      prevState: initialState,
-    });
+        const [iState, setIState] = React.useState<InteractiveStateChange>({
+          state: initialState,
+          prevState: initialState,
+        });
 
-    // onStateChange prop callback
-    React.useEffect(
-      () => {
-        if (
-          onStateChange &&
-          // check if stateChanged so onStateChange is not called on the initial render
-          // which is the only time iState.state deep=== iState.prevState
-          stateChanged(iState)
-        ) {
-          onStateChange(iState);
-        }
-      },
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [
-        iState.state.hover,
-        iState.state.active,
-        iState.state.focus,
-        iState.state.disabled,
-      ],
-    );
-
-    const keyTracking = React.useRef<{
-      enterKeyDown: boolean;
-      spaceKeyDown: boolean;
-    }>({ enterKeyDown: false, spaceKeyDown: false });
-
-    const touchActiveTimeoutId = React.useRef<number | undefined>(undefined);
-    React.useEffect(
-      () => () => window.clearTimeout(touchActiveTimeoutId.current),
-      [],
-    );
-
-    interface HoverStateChange {
-      iKey: 'hover';
-      state: boolean;
-      action: 'enter' | 'exit';
-    }
-    interface ActiveStateChange {
-      iKey: 'active';
-      state: ActiveState;
-      action: 'enter' | 'exit';
-    }
-    interface FocusStateChange {
-      iKey: 'focus';
-      state: FocusState;
-      action: 'enter' | 'exit';
-    }
-    interface DisabledStateChange {
-      iKey: 'disabled';
-      state: boolean;
-      action: 'enter' | 'exit';
-    }
-    type StateChangeFunction = (
-      changes: (
-        | HoverStateChange
-        | ActiveStateChange
-        | FocusStateChange
-        | DisabledStateChange
-      )[],
-    ) => void;
-    // stateChange is idempotent so event handlers can be dumb (don't need to know current state)
-    // for example mousedown and pointerdown event handlers
-    // will both call stateChange with action enter mouseActive state
-    const stateChange: StateChangeFunction = React.useCallback(
-      (changes) => {
-        // first check if changes are different from current state
-        if (
-          changes.some(({ iKey, state, action }) =>
-            action === 'enter'
-              ? iState.state[iKey] !== state
-              : iState.state[iKey] === state,
-          )
-        ) {
-          setIState((previous) => {
-            const newState = { ...previous.state };
-            changes.forEach(({ iKey, state, action }) => {
-              if (action === 'enter') {
-                // TS should known that iKey and state values are matched to each other
-                // based on the StateChangeFunction type above, but TS doesn't understand this
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                newState[iKey] = state;
-              } else if (action === 'exit' && previous.state[iKey] === state) {
-                newState[iKey] = false;
-              }
-            });
-            const newInteractiveStateChange = {
-              state: newState,
-              prevState: previous.state,
-            };
-            return stateChanged(newInteractiveStateChange)
-              ? newInteractiveStateChange
-              : previous;
-          });
-        }
-      },
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [
-        iState.state.hover,
-        iState.state.active,
-        iState.state.focus,
-        iState.state.disabled,
-      ],
-    );
-
-    const handleEvent = React.useCallback(
-      (e: React.UIEvent) => {
-        // call event handler for the current event if it is passed in as a prop
-        if (restProps[eventMap[e.type]]) {
-          restProps[eventMap[e.type]](e);
-        }
-
-        // TODO nested switch
-
-        if (eventFrom(e) === 'mouse') {
-          if (e.type === 'mouseenter') {
-            stateChange([{ iKey: 'hover', state: true, action: 'enter' }]);
-          } else if (e.type === 'mouseleave') {
-            stateChange([{ iKey: 'hover', state: false, action: 'enter' }]);
-          }
-        }
-      },
-      // handleEvent is only dependent on event handler props that are also in eventMap
-      // for example, restProps.onMouseEnter, restProps.onTouchStart, etc
-      // useCallback so handleEvent is not re-created if event handler props are unchanged (e.g. on Interactive state change)
-      // and also so handleEvent can be used as a dependent in useMemo/useCallback, e.g. eventListeners
-      // this generates an array of event handler props that are also in eventMap
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [
-        ...Object.keys(eventMap).map((key) => restProps[eventMap[key]]),
-        stateChange,
-      ],
-    );
-
-    // create object with event listeners to pass to <As>
-    const eventListeners = React.useMemo(
-      () =>
-        Object.keys(eventMap).reduce(
-          (
-            objWithListeners: Record<string, React.EventHandler<any>>,
-            listenerPropName,
-          ) => {
-            objWithListeners[eventMap[listenerPropName]] = handleEvent;
-            return objWithListeners;
+        // onStateChange prop callback
+        React.useEffect(
+          () => {
+            if (
+              onStateChange &&
+              // check if stateChanged so onStateChange is not called on the initial render
+              // which is the only time iState.state deep=== iState.prevState
+              stateChanged(iState)
+            ) {
+              onStateChange(iState);
+            }
           },
-          {},
-        ),
-      [handleEvent],
-    );
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          [
+            iState.state.hover,
+            iState.state.active,
+            iState.state.focus,
+            iState.state.disabled,
+          ],
+        );
 
-    // support ref prop as object or callback, and track ref locally
-    const localRef = React.useRef<HTMLElement | null>(null);
-    const callbackRef = React.useCallback(
-      (node: HTMLElement | null) => {
-        localRef.current = node;
-        if (typeof ref === 'function') {
-          ref(node);
-        } else if (ref) {
-          ref.current = node;
+        const keyTracking = React.useRef<{
+          enterKeyDown: boolean;
+          spaceKeyDown: boolean;
+        }>({ enterKeyDown: false, spaceKeyDown: false });
+
+        const touchActiveTimeoutId = React.useRef<number | undefined>(
+          undefined,
+        );
+        React.useEffect(
+          () => () => window.clearTimeout(touchActiveTimeoutId.current),
+          [],
+        );
+
+        interface HoverStateChange {
+          iKey: 'hover';
+          state: boolean;
+          action: 'enter' | 'exit';
         }
-      },
-      [ref],
-    );
+        interface ActiveStateChange {
+          iKey: 'active';
+          state: ActiveState;
+          action: 'enter' | 'exit';
+        }
+        interface FocusStateChange {
+          iKey: 'focus';
+          state: FocusState;
+          action: 'enter' | 'exit';
+        }
+        interface DisabledStateChange {
+          iKey: 'disabled';
+          state: boolean;
+          action: 'enter' | 'exit';
+        }
+        type StateChangeFunction = (
+          changes: (
+            | HoverStateChange
+            | ActiveStateChange
+            | FocusStateChange
+            | DisabledStateChange
+          )[],
+        ) => void;
+        // stateChange is idempotent so event handlers can be dumb (don't need to know current state)
+        // for example mousedown and pointerdown event handlers
+        // will both call stateChange with action enter mouseActive state
+        const stateChange: StateChangeFunction = React.useCallback(
+          (changes) => {
+            // first check if changes are different from current state
+            if (
+              changes.some(({ iKey, state, action }) =>
+                action === 'enter'
+                  ? iState.state[iKey] !== state
+                  : iState.state[iKey] === state,
+              )
+            ) {
+              setIState((previous) => {
+                const newState = { ...previous.state };
+                changes.forEach(({ iKey, state, action }) => {
+                  if (action === 'enter') {
+                    // TS should known that iKey and state values are matched to each other
+                    // based on the StateChangeFunction type above, but TS doesn't understand this
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    newState[iKey] = state;
+                  } else if (
+                    action === 'exit' &&
+                    previous.state[iKey] === state
+                  ) {
+                    newState[iKey] = false;
+                  }
+                });
+                const newInteractiveStateChange = {
+                  state: newState,
+                  prevState: previous.state,
+                };
+                return stateChanged(newInteractiveStateChange)
+                  ? newInteractiveStateChange
+                  : previous;
+              });
+            }
+          },
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          [
+            iState.state.hover,
+            iState.state.active,
+            iState.state.focus,
+            iState.state.disabled,
+          ],
+        );
 
-    return (
-      <As
-        ref={callbackRef}
-        {...restProps}
-        {...eventListeners}
-        style={{ color: 'blue', display: 'block' }}
-      >
-        {typeof children === 'function' ? children(iState.state) : children}
-      </As>
-    );
-  },
-);
+        const handleEvent = React.useCallback(
+          (e: React.UIEvent) => {
+            // call event handler for the current event if it is passed in as a prop
+            if (restProps[eventMap[e.type]]) {
+              restProps[eventMap[e.type]](e);
+            }
+
+            // TODO nested switch
+
+            if (eventFrom(e) === 'mouse') {
+              if (e.type === 'mouseenter') {
+                stateChange([{ iKey: 'hover', state: true, action: 'enter' }]);
+              } else if (e.type === 'mouseleave') {
+                stateChange([{ iKey: 'hover', state: false, action: 'enter' }]);
+              }
+            }
+          },
+          // handleEvent is only dependent on event handler props that are also in eventMap
+          // for example, restProps.onMouseEnter, restProps.onTouchStart, etc
+          // useCallback so handleEvent is not re-created if event handler props are unchanged (e.g. on Interactive state change)
+          // and also so handleEvent can be used as a dependent in useMemo/useCallback, e.g. eventListeners
+          // this generates an array of event handler props that are also in eventMap
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          [
+            ...Object.keys(eventMap).map((key) => restProps[eventMap[key]]),
+            stateChange,
+          ],
+        );
+
+        // create object with event listeners to pass to <As>
+        const eventListeners = React.useMemo(
+          () =>
+            Object.keys(eventMap).reduce(
+              (
+                objWithListeners: Record<string, React.EventHandler<any>>,
+                listenerPropName,
+              ) => {
+                objWithListeners[eventMap[listenerPropName]] = handleEvent;
+                return objWithListeners;
+              },
+              {},
+            ),
+          [handleEvent],
+        );
+
+        // support ref prop as object or callback, and track ref locally
+        const localRef = React.useRef<HTMLElement | null>(null);
+        const callbackRef = React.useCallback(
+          (node: HTMLElement | null) => {
+            localRef.current = node;
+            if (typeof ref === 'function') {
+              ref(node);
+            } else if (ref) {
+              ref.current = node;
+            }
+          },
+          [ref],
+        );
+
+        return (
+          <As
+            ref={callbackRef}
+            {...restProps}
+            {...eventListeners}
+            style={{ color: 'blue', display: 'block' }}
+          >
+            {typeof children === 'function' ? children(iState.state) : children}
+          </As>
+        );
+      },
+    ),
+  );
