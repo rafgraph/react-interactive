3eadae244732dce75985ad23ec4e0961d376cee9
Rafael Pedicini <rafael@rafgraph.dev>
2021-04-04 20:01:06 -0400

    Update demo to use react-interactive@1.0.0-next.4

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,13 +1,13 @@
-// import React so the demo app is compatible with code sandbox
-// which doesn't support the new JSX transform yet
-// see https://github.com/codesandbox/codesandbox-client/issues/5307
-import React, { StrictMode } from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
